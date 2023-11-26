e49c41328e9fa634339a75852a13ba647dc197f9
Fabian Ramos <fabian2103@gmail.com>
2023-11-26 16:59:26 -0500

    Next commit

--- src/config/environments/environments.js
+++ src/config/environments/environments.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 exports.envs = {
   PORT: env.get("PORT").required().asPortNumber(),
+  DB_URI: env.get("DB_URI").required().asUrlString(),
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 const app = require("./app.js");
+const { authenticated, syncUp } = require("./config/database/database.js");
 const { envs } = require("./config/environments/environments.js");
+
+async function main() {
+  try {
+    await authenticated();
+    await syncUp();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
 
 app.listen(envs.PORT, () => {
   console.log("Server is running on port" + envs.PORT);
